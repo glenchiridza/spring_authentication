db2281459ef998c1a5c0fe08ef94e77f4a7d5d14
glenchiridza <glenchiridz@gmail.com>
2023-01-11 01:27:08 +0200

    database configurations

---
+++ src/test/java/com/glencconnect/springbootauthenticationthymeleaf/SpringBootAuthenticationThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glencconnect.springbootauthenticationthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAuthenticationThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
