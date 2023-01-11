6c31e4c9036ea723cbbed8821c5988f53e81a8d2
glenchiridza <glenchiridz@gmail.com>
2023-01-11 23:29:55 +0200

    role model

--- src/main/java/com/glencconnect/springbootauthenticationthymeleaf/model/Role.java
+++ src/main/java/com/glencconnect/springbootauthenticationthymeleaf/model/Role.java
@@ -1,4 +1,30 @@
 package com.glencconnect.springbootauthenticationthymeleaf.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "role")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
