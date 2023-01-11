af9923b935e4ca407689836a4aa599a3f291ecfb
glenchiridza <glenchiridz@gmail.com>
2023-01-11 23:28:22 +0200

    user fields

--- src/main/java/com/glencconnect/springbootauthenticationthymeleaf/model/User.java
+++ src/main/java/com/glencconnect/springbootauthenticationthymeleaf/model/User.java
@@ -1,10 +1,19 @@
 package com.glencconnect.springbootauthenticationthymeleaf.model;
 
+
+import jakarta.persistence.*;
+
 import java.util.Collection;
 
+@Entity
+@Table(name="users",uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
     private String email;
     private String password;
