921e3c2f37e7a9ea16308d81d81842d25c720410
glenchiridza <glenchiridz@gmail.com>
2023-01-12 23:03:21 +0200

    user registration controller

--- src/main/java/com/glencconnect/springbootauthenticationthymeleaf/web/controller/UserRegistrationController.java
+++ src/main/java/com/glencconnect/springbootauthenticationthymeleaf/web/controller/UserRegistrationController.java
@@ -4,7 +4,9 @@ import com.glencconnect.springbootauthenticationthymeleaf.service.UserServiceImp
 import com.glencconnect.springbootauthenticationthymeleaf.web.dto.UserRegistrationDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,6 +20,12 @@ public class UserRegistrationController {
         this.userService = userService;
     }
 
+    @GetMapping
+    public String showRegistrationForm(){
+        return "registration";
+    }
+
+    @PostMapping
     public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto){
         userService.save(registrationDto);
         return "redirect:/registration?success";
