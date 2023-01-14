5e1159ad287aaf1851fcebe5e9edb1d78ed45cd8
glenchiridza <glenchiridz@gmail.com>
2023-01-15 00:53:44 +0200

    map user to roles

--- src/main/java/com/glencconnect/springbootauthenticationthymeleaf/service/UserServiceImpl.java
+++ src/main/java/com/glencconnect/springbootauthenticationthymeleaf/service/UserServiceImpl.java
@@ -5,10 +5,16 @@ import com.glencconnect.springbootauthenticationthymeleaf.model.User;
 import com.glencconnect.springbootauthenticationthymeleaf.repository.UserRepository;
 import com.glencconnect.springbootauthenticationthymeleaf.web.dto.UserRegistrationDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -31,5 +37,20 @@ public class UserServiceImpl implements UserService{
         );
 
         return userRepository.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if(user == null){
+            throw new UsernameNotFoundException("Invalid username or password");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(),mapRolesToAuthorities(user.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles){
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
     }
 }
