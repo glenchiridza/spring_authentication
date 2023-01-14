3af8f79acaf1fb011d88602ea45b928987856d2b
glenchiridza <glenchiridz@gmail.com>
2023-01-15 00:31:05 +0200

    custom login configurations

--- src/main/java/com/glencconnect/springbootauthenticationthymeleaf/config/SecurityConfiguration.java
+++ src/main/java/com/glencconnect/springbootauthenticationthymeleaf/config/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -20,5 +21,29 @@ public class SecurityConfiguration{
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception{
+        http.
+                csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/registration**",
+                        "/js/**",
+                        "/css/**",
+                        "/img/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout")
+                .permitAll();
+        return http.build();
+    }
 
 }
